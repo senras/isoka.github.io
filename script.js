677fff78e430f717fe1cba0ef189b329c0f834a3
Ignacio Senra Oka <isenras@hotmail.com>
2023-10-22 08:27:46 -0300

    Blue totoro added at footer

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function scrollToElement(elementSelector, instance = 0) {
 
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
 
 link1.addEventListener('click', () => {
     scrollToElement('.header');
@@ -19,8 +18,4 @@ link1.addEventListener('click', () => {
 link2.addEventListener('click', () => {
     // Scroll to the second element with "header" class
     scrollToElement('.header', 1);
-});
-
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
 });
